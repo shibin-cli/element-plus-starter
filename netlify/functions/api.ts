1416993d3e838b5da8966f86b9688a5c1659c608
shibin <shibin.you@qq.com>
2024-07-24 18:47:46 +0800

    ci: 🎡 测试部署 express

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,16 +1,16 @@
 import express, { Router } from 'express'
 import serverless from 'serverless-http'
-import mockMethods from '../../mock'
+// import mockMethods from '../../mock'
 const api = express()
 
 const router = Router()
-mockMethods.forEach((config) => {
-  router.get(config.method)
-  router[config.method](config.url, (res) => {
-    res.send(config.response())
-  })
-})
-router.get('/hello', (req, res) => res.send('Hello World!'))
+// mockMethods.forEach((config) => {
+//   router.get(config.method)
+//   router[config.method](config.url, (res) => {
+//     res.send(config.response())
+//   })
+// })
+router.get('/hello', (_, res) => res.send('Hello World!'))
 
 api.use('/', router)
 
